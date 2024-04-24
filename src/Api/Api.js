0df89c2b5c532d495c9f0e4d3f5b571d5c6ca2d3
Deepak ts <tsdeepak589@gmail.com>
2024-04-24 18:08:47 +0530

    image problem solved

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,8 +1,8 @@
 
 
-export const adminApi='http://localhost:5001/admin'
-export const userApi='http://localhost:5001'
+// export const adminApi='http://localhost:5001/admin'
+// export const userApi='http://localhost:5001'
 
 
-// export const userApi='https://backend.studycap.in'
-// export const adminApi='https://backend.studycap.in/admin'
+export const userApi='https://backend.studycap.in'
+export const adminApi='https://backend.studycap.in/admin'
